248a0c9d52458967d73eb7039063e904b299857d
fsamji07 <fatimah.samji@city.ac.uk>
2024-10-27 13:58:59 +0000

    Add files via upload

---
+++ HW 4/pets.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE petPet (
+  petname NVARCHAR(20) NOT NULL,
+  owner NVARCHAR(45) NOT NULL,
+  species NVARCHAR(45) NOT NULL,
+  gender NVARCHAR(1) NOT NULL,
+  birth date NOT NULL,
+  death date NOT NULL,
+  PRIMARY KEY petname
+);
+
+CREATE TABLE petEvent (
+  petname NVARCHAR(20) NOT NULL,
+  eventdate date NOT NULL,
+  eventtype NVARCHAR(15) NOT NULL,
+  remark NVARCHAR(255),
+  FOREIGN KEY petname,
+  PRIMARY KEY eventdate, eventtype
+);
